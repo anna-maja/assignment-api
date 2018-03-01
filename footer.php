a7a057ea82048826ae8bc0544c1ae511cc46556d
rsafrid <33651630+rsafrid@users.noreply.github.com>
2018-03-01 14:59:43 +0100

    Inte klara ännu

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <?php
+    echo "Copyright &copy; Anna-Maja Lithner, Tobias Hedkvist och Susanne Fridh &bull;";
+    echo date("Y");
+    echo "<br>Tomtebodavägen 3A &bull; Solna &bull; Mobil: 070-321 54 35";
+    ?>
+</footer>
+</div>
+    
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Har lagt till meta-tag för description  -->
+    <meta name="description" content="Webbsida om mitt företag Susannes webshop. Du kan köpa halsduk, vantar och sockor.">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- Länk till bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Länk till google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
+    <!-- Länk till CSS-dokumentet  -->
+    <link rel="stylesheet" href="stil.css"> 
+    <!-- Liten bild på sid-fliken -->
+    <link rel="shortcut icon" href="bilder/garn2.png" />
+    <!-- Title sätts till Susannes webshop -->
+    <title><?=SITE_TITLE?></title>
+ 
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Hem</a>
+  <a href="#news">Boka film</a>
+  <a href="#contact">Kontakt</a>
+</div>
+<div class="container">
+    <div class="jumbotron">
+<!-- Denna rubrik finns med på alla sidor -->
+<h1>Välkommen till vår webshop</h1>
+    </div>
