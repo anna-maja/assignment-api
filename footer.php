97335100bf72cd5b7efde6b6d5e19601d5054b27
rsafrid <33651630+rsafrid@users.noreply.github.com>
2018-03-05 11:14:03 +0100

    Uppdaterade /Susanne

--- footer.php
+++ footer.php
@@ -1,11 +1,84 @@
-<footer>
-<p>Inlämningsuppgift 2 i Backend, KVALIT17, 2018-03-02</p>
-    <p> Copyright &copy; Anna-Maja Lithner, Tobias Hedkvist och Susanne Fridh &bull;
-    <?=date("Y")?><br>
-    Tomtebodavägen 3A &bull; Solna &bull; Mobil: 070-321 54 35
-    </p>
-</footer>
-</div> 
-    
-</body>
+<!-- Footer -->
+<footer class="footer text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Retrobutiken</h4>
+            <p class="lead mb-0">Tomtebodavägen 3A
+              <br>171 65 Solna
+            <br>Mail: retrobutiken@webshopen.se
+            <br>Telefon: 08-88 88 88</p>
+          </div>
+          <div class="col-md-4">
+            <hr class="star-light mb-5">
+            <br><br><br>
+            <a href="viewOrders.php">Admin login</a>
+          </div>
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Här kan du följa oss</h4>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-google-plus"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-linkedin"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-dribbble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small>Copyright &copy; <?= date("Y");?> <br> Tobias Hedkvist &bull; Anna-Maja Lithner &bull; Susanne Fridh</small>
+      </div>
+    </div>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-to-top d-lg-none position-fixed ">
+      <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
+
+    <!-- Portfolio Modals -->
+
+  
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/freelancer.min.js"></script>
+
+  </body>
+
 </html>
